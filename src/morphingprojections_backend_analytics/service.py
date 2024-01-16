e515b1c26cd0074c43100a3a0080b2649b3cae40
Miguel Salinas Gancedo <masalinas.gancedo@gmail.com>
2024-01-16 17:08:12 +0100

    last commit

--- src/morphingprojections_backend_analytics/service.py
+++ src/morphingprojections_backend_analytics/service.py
@@ -20,11 +20,15 @@ References:
     - https://pip.pypa.io/en/stable/reference/pip_install
 """
 
+import os
 import argparse
 import logging
 import sys
 
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+#from flask.ext.cors import CORS
+
+from elasticsearch import Elasticsearch
 
 __author__ = "Miguel Salinas Gancedo"
 __copyright__ = "Miguel Salinas Gancedo"
@@ -32,7 +36,24 @@ __license__ = "MIT"
 
 _logger = logging.getLogger(__name__)
 
+# Project default configuration
+PROJECT_ID = "genomic"
+INDEX_DATAMATRIX = "dataset_datamatrix"
+
+# Database default configuration
+#ELASTIC_HOST = "https://avib-elastic:9200"
+ELASTIC_HOST = "https://avispe.edv.uniovi.es:443/kubernetes/elastic"
+ELASTIC_USER = "elastic"
+ELASTIC_PASSWORD = "password"
+#ELASTIC_CERTIFICATE = os.path.dirname(os.path.realpath(__file__)) + "/certificates/ca-elastic-local.crt"
+ELASTIC_CERTIFICATE = os.path.dirname(os.path.realpath(__file__)) + "/certificates/ca-elastic-avib.crt"
+
 app = Flask(__name__)
+#CORS(app)
+
+# private module attributes
+_index_name = PROJECT_ID + "_" + INDEX_DATAMATRIX
+_connection_db = None
 
 def parse_args(args):
     """Parse command line parameters
@@ -74,6 +95,24 @@ def setup_logging(loglevel):
         level=loglevel, stream=sys.stdout, format=logformat, datefmt="%Y-%m-%d %H:%M:%S"
     )
 
+def connect_database(**kwargs):    
+    # Create databse client connection and debug
+    _connection_db = Elasticsearch(
+        ELASTIC_HOST,
+        basic_auth=(ELASTIC_USER, ELASTIC_PASSWORD),
+        ca_certs=ELASTIC_CERTIFICATE,
+        verify_certs=True,
+        http_compress=True
+    )
+
+    try:
+        _logger.debug("Database info: %s", _connection_db.info())
+    except ConnectionError as exception:
+        _logger.error("Error database message %s from %s", exception.message, args.elastic_host)
+        sys.exit()
+
+    return _connection_db
+
 @app.route('/')
 def default_route():
     """Default route"""
@@ -87,18 +126,51 @@ def default_route():
 
     return jsonify('hello world')
 
+@app.route('/tsne',  methods=['POST'])
+def tsne():
+    global _connection_db
+    global _index_name
+
+    data = request.get_json() 
+
+    response = []
+
+    attributes = data['attributes']
+    samples = data['samples']
+
+    # filter by samples and attributes in elastic    
+    results = _connection_db.search(index=_index_name, query={
+        "terms": { "attribute": attributes}        
+    })    
+
+    # fill will data filtering
+    for hit in results['hits']['hits']:
+        response.append(hit["_source"])
+
+    return jsonify(result=response)
+
 def main(args):
+    global _connection_db
+
     args, unknown = parse_args(args)
     
     setup_logging(args.loglevel)
     
+    # connect to elastic database
+    _connection_db = connect_database()
+
     _logger.info("Starting service ...")
     app.run(host='0.0.0.0', port=args.port, debug=True)
     _logger.info("Service ends here")
 
     return app
 
-def wsgi():        
+def wsgi():
+    global _connection_db
+
+    # connect to elastic database
+    _connection_db = connect_database()
+
     return app
 
 def run():
