3cacd5cceafba02c957d94e088570fc1307ae2c4
Miguel Salinas Gancedo <masalinas.gancedo@gmail.com>
2024-01-19 19:24:32 +0100

    last commit

--- src/morphingprojections_backend_analytics/service.py
+++ src/morphingprojections_backend_analytics/service.py
@@ -24,6 +24,9 @@ import os
 import argparse
 import logging
 import sys
+from operator import attrgetter
+
+import numpy as np
 import pandas as pd
 
 from flask import Flask, jsonify, request
@@ -42,23 +45,28 @@ _logger = logging.getLogger(__name__)
 
 # Project default configuration
 PROJECT_ID = "genomic"
-INDEX_DATAMATRIX = "dataset_datamatrix"
+INDEX_DATAMATRIX = "dataset_datamatrix_new"
+INDEX_DATASET_SAMPLE_VIEW = "dataset_encoding_default_new"
+INDEX_DATASET_ATTRIBUTE_VIEW = "dataset_attribute_view_encoding_default_new"
 INDEX_SCROLL_MAX_TIME = '10s'
 INDEX_MATCH_SIZE = 9000
 
 # Database default configuration
-#ELASTIC_HOST = "https://avib-elastic:9200"
-ELASTIC_HOST = "https://avispe.edv.uniovi.es:443/kubernetes/elastic"
+ELASTIC_HOST = "https://avib-elastic:9200"
+#ELASTIC_HOST = "https://avispe.edv.uniovi.es:443/kubernetes/elastic"
 ELASTIC_USER = "elastic"
 ELASTIC_PASSWORD = "password"
-#ELASTIC_CERTIFICATE = os.path.dirname(os.path.realpath(__file__)) + "/certificates/ca-elastic-local.crt"
-ELASTIC_CERTIFICATE = os.path.dirname(os.path.realpath(__file__)) + "/certificates/ca-elastic-avib.crt"
+ELASTIC_CERTIFICATE = os.path.dirname(os.path.realpath(__file__)) + "/certificates/ca-elastic-local.crt"
+#ELASTIC_CERTIFICATE = os.path.dirname(os.path.realpath(__file__)) + "/certificates/ca-elastic-avib.crt"
 
 app = Flask(__name__)
 #CORS(app)
 
 # private module attributes
-_index_name = PROJECT_ID + "_" + INDEX_DATAMATRIX
+_index_datamatrix = PROJECT_ID + "_" + INDEX_DATAMATRIX
+_index_dataset_sample_view = PROJECT_ID + "_" + INDEX_DATASET_SAMPLE_VIEW
+_index_dataset_attribute_view = PROJECT_ID + "_" + INDEX_DATASET_ATTRIBUTE_VIEW
+
 _connection_db = None
 
 def parse_args(args):
@@ -119,6 +127,7 @@ def connect_database(**kwargs):
 
     return _connection_db
 
+'''
 @app.route('/')
 def default_route():
     """Default route"""
@@ -131,11 +140,183 @@ def default_route():
     _logger.info("INFO LOG")
 
     return jsonify('hello world')
+'''
+
+def filter_index_datamatrix(view, items, filter_sample=None, filter_attribute=None):
+    global _connection_db
+    global _index_datamatrix
+
+    response = []
+
+    body = {
+            "size": INDEX_MATCH_SIZE,
+            "query": {
+                "bool": {
+                    "must": []
+                }
+            }
+        }
+
+    if view == "sample_view":
+        body["query"]["bool"]["must"].append({"terms": {"sample_id": items }})
+
+        if (filter_attribute is not None):
+            body["query"]["bool"]["must"].append({"term": {"attribute": filter_attribute }})
+
+    if view == "attribute_view":
+        body["query"]["bool"]["must"].append({"terms": {"attribute": items }})
+
+        if (filter_attribute is not None):
+            body["query"]["bool"]["must"].append({"term": {"sample_id": filter_sample }})
+
+    resp = _connection_db.search(
+        index=_index_datamatrix, 
+        body = body,
+        scroll = INDEX_SCROLL_MAX_TIME)
+
+    # keep track of pass scroll _id
+    old_scroll_id = resp['_scroll_id']
+
+    # use a 'while' iterator to loop over document 'hits'
+    expression_lst = []
+
+    # iterate over the document hits for each 'scroll'
+    while len(resp['hits']['hits']):        
+        doc_count = 0
+        for doc in resp['hits']['hits']:
+            expression_lst.append(doc['_source'])
+
+            doc_count += 1            
+
+            _logger.info("DOC COUNT: %s", doc_count)
+
+        _logger.info("TOTAL DOC COUNT: %s", doc_count)
+
+        # make a request using the Scroll API
+        resp = _connection_db.scroll(
+            scroll_id = old_scroll_id,
+            scroll = INDEX_SCROLL_MAX_TIME # length of time to keep search context
+        )
+
+        # check if there's a new scroll ID
+        if old_scroll_id != resp['_scroll_id']:
+            _logger.info("NEW SCROLL ID: %s", resp['_scroll_id'])
+
+        # keep track of pass scroll _id
+        old_scroll_id = resp['_scroll_id']
+
+    return expression_lst
+
+def filter_index_sample_view(items):
+    global _connection_db
+    global _index_dataset_sample_view
+
+    response = []
+
+    body = {
+            "size": INDEX_MATCH_SIZE,
+            "query": {
+                "terms": {
+                    "sample_id": items
+                }
+            }
+        }
+
+    resp = _connection_db.search(
+        index=_index_dataset_sample_view, 
+        body = body,
+        scroll = INDEX_SCROLL_MAX_TIME)
+
+    # keep track of pass scroll _id
+    old_scroll_id = resp['_scroll_id']
+
+    # use a 'while' iterator to loop over document 'hits'
+    expression_lst = []
+
+    # iterate over the document hits for each 'scroll'
+    while len(resp['hits']['hits']):        
+        doc_count = 0
+        for doc in resp['hits']['hits']:
+            expression_lst.append(doc['_source'])
+
+            doc_count += 1            
+
+            _logger.info("DOC COUNT: %s", doc_count)
+
+        _logger.info("TOTAL DOC COUNT: %s", doc_count)
+
+        # make a request using the Scroll API
+        resp = _connection_db.scroll(
+            scroll_id = old_scroll_id,
+            scroll = INDEX_SCROLL_MAX_TIME # length of time to keep search context
+        )
+
+        # check if there's a new scroll ID
+        if old_scroll_id != resp['_scroll_id']:
+            _logger.info("NEW SCROLL ID: %s", resp['_scroll_id'])
+
+        # keep track of pass scroll _id
+        old_scroll_id = resp['_scroll_id']
+
+    return expression_lst
+
+def filter_index_attribute_view(items):
+    global _connection_db
+    global _index_dataset_attribute_view
+
+    response = []
+
+    body = {
+            "size": INDEX_MATCH_SIZE,
+            "query": {
+                "terms": {
+                    "attribute": items
+                }
+            }
+        }
+
+    resp = _connection_db.search(
+        index=_index_dataset_attribute_view, 
+        body = body,
+        scroll = INDEX_SCROLL_MAX_TIME)
+
+    # keep track of pass scroll _id
+    old_scroll_id = resp['_scroll_id']
+
+    # use a 'while' iterator to loop over document 'hits'
+    expression_lst = []
+
+    # iterate over the document hits for each 'scroll'
+    while len(resp['hits']['hits']):        
+        doc_count = 0
+        for doc in resp['hits']['hits']:
+            expression_lst.append(doc['_source'])
+
+            doc_count += 1            
+
+            _logger.info("DOC COUNT: %s", doc_count)
+
+        _logger.info("TOTAL DOC COUNT: %s", doc_count)
+
+        # make a request using the Scroll API
+        resp = _connection_db.scroll(
+            scroll_id = old_scroll_id,
+            scroll = INDEX_SCROLL_MAX_TIME # length of time to keep search context
+        )
+
+        # check if there's a new scroll ID
+        if old_scroll_id != resp['_scroll_id']:
+            _logger.info("NEW SCROLL ID: %s", resp['_scroll_id'])
+
+        # keep track of pass scroll _id
+        old_scroll_id = resp['_scroll_id']
+
+    return expression_lst
 
 @app.route('/tsne',  methods=['POST'])
 def tsne():
     global _connection_db
-    global _index_name
+    global _index_datamatrix
 
     response = []
     data = request.get_json() 
@@ -146,10 +327,9 @@ def tsne():
     samples = data['samples']
     attributes = data['attributes']
 
-    # filter by samples and attributes in database
-    _logger.info("Create database filter to recover samples from elasticsearch for %s filter", name)
+    _logger.info("Create database body to recover samples from elasticsearch for %s filter", name)
 
-    filter = {
+    body = {
             "size": INDEX_MATCH_SIZE,
             "query": {
                 "bool": {
@@ -159,16 +339,16 @@ def tsne():
         }
 
     if len(samples) > 0:
-        filter["query"]["bool"]["must"].append({"terms": {"sample_id": samples }})
+        body["query"]["bool"]["must"].append({"terms": {"sample_id": samples }})
 
     if len(attributes) > 0:
-        filter["query"]["bool"]["must"].append({"terms": {"attribute": attributes }})
+        body["query"]["bool"]["must"].append({"terms": {"attribute": attributes }})
 
     _logger.info("Execute filter from elasticsearch in bulk requests")
 
     resp = _connection_db.search(
-        index=_index_name, 
-        body = filter,
+        index=_index_datamatrix, 
+        body = body,
         scroll = INDEX_SCROLL_MAX_TIME)
 
     # keep track of pass scroll _id
@@ -206,7 +386,8 @@ def tsne():
     _logger.info("Parse request to fit t-SNE model")
 
     df = pd.DataFrame(expression_lst)
-    df = df.drop("sample_type", axis = 1)
+    #df = df.drop("sample_type", axis = 1)
+    df = df.drop("data_type", axis = 1)
     df = df.drop("cancer_code", axis = 1)
     
     dataset_expression = df.pivot(index='sample_id', columns='attribute', values='value')
@@ -237,6 +418,95 @@ def tsne():
     _logger.info(len(dataset_projection_lst))
 
     return dataset_projection_lst
+
+@app.route('/histogram',  methods=['POST'])
+def histogram():
+    response = []
+    data = request.get_json() 
+
+    # recover request data
+    name = data["name"]
+    title = data["title"] 
+    view = data["view"]
+    annotation = None
+    if "annotation" in data:
+        annotation = data["annotation"]
+    filter_sample = None
+    if "filter_sample" in data:   
+        filter_sample = data["filter_sample"]
+    filter_attribute = None
+    if "filter_attribute" in data:           
+        filter_attribute = data["filter_attribute"]   
+    if "bins" in data:
+        bins = data["bins"]
+    groups = data['groups']
+
+    items = []
+    for group in groups:
+        items = items + group["values"]
+
+    # get expression list from filters    
+    _logger.info("Get filter items from data view %s for %s histogram", view, name)
+
+    expression_lst = None
+    if view == "sample_view":
+        if filter_attribute is not None:
+            expression_lst = filter_index_datamatrix(view, items, filter_sample, filter_attribute)
+        else: 
+            expression_lst = filter_index_sample_view(items)
+
+    if view == "attribute_view":
+        if filter_sample is not None:
+            expression_lst = filter_index_datamatrix(view, items, filter_sample, filter_attribute)
+        else: 
+            expression_lst = filter_index_attribute_view(items)
+
+    # add group/color to all expressions list
+    for expression in expression_lst:
+        for group in groups:
+            for value in group["values"]:
+                if value == expression["sample_id"]:
+                    expression["group"] = group["name"]
+                    expression["color"] = group["color"]
+
+                    break
+
+    # group expression list by annotation
+    _logger.info("Group filter items from data view %s for %s histogram", view, name)
+
+    if annotation is not None:
+        expression_df = pd.DataFrame(expression_lst)
+
+        expression_grouped_df = expression_df.groupby([annotation, "group", "color"])[annotation].count()
+    else:
+        max_expression = max(expression_lst, key=lambda exp: exp["value"])
+        min_expression = min(expression_lst, key=lambda exp: exp["value"])        
+        interval = (max_expression["value"] - min_expression["value"]) / bins
+
+        for expression in expression_lst:
+            for bin in range(0, bins):
+                if (expression["value"] < min_expression["value"] + interval * bin):
+                    #expression["bin"] = bin - 1
+                    expression["bin"] = min_expression["value"] + bin * interval                    
+                    break
+
+        expression_df = pd.DataFrame(expression_lst)
+        
+        expression_grouped_df = expression_df.groupby(["bin", "group", "color"])["bin"].count()
+
+    # parse respose to json list
+    for annotation in expression_grouped_df.index:
+        response.append({
+            "annotation": annotation[0],
+            "group": annotation[1], 
+            "color": annotation[2], 
+            "value": int(expression_grouped_df.loc[annotation])
+        })
+
+    # order group respose by annotation
+    response.sort(key=lambda item: item["annotation"])    
+
+    return response
 
 def main(args):
     global _connection_db
